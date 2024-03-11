df380737488d1c25bce8e638583ce4ca21739b2a
Kamil Michna <kamil.michna28@gmail.com>
2024-03-11 16:06:41 +0100

    progress

--- src/app/agent-details-accordion/agent-details-accordion.component.ts
+++ src/app/agent-details-accordion/agent-details-accordion.component.ts
@@ -3,20 +3,19 @@ import { Agent } from '../agents.service';
 import { DatePipe } from '@angular/common';
 import { FileTreeComponent } from '../file-tree/file-tree.component';
 import { RouterModule } from '@angular/router';
+import { ChatWindowComponent } from "../chat-window/chat-window.component";
 
 @Component({
-  selector: 'app-agent-details-accordion',
-  standalone: true,
-  imports: [DatePipe,FileTreeComponent,RouterModule],
-  templateUrl: './agent-details-accordion.component.html',
-  styleUrl: './agent-details-accordion.component.scss'
+    selector: 'app-agent-details-accordion',
+    standalone: true,
+    templateUrl: './agent-details-accordion.component.html',
+    styleUrl: './agent-details-accordion.component.scss',
+    imports: [DatePipe, FileTreeComponent, RouterModule, ChatWindowComponent]
 })
 export class AgentDetailsAccordionComponent implements OnInit {
     @Input('agent') agent?: Agent;
-    
 
     ngOnInit(){
-      
-    }
 
+    }
 }
