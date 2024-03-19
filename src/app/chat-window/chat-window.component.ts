baf1edbebb649b859ca096679472e62205aa5cf3
Kamil Michna <kamil.michna28@gmail.com>
2024-03-19 22:22:20 +0100

    progress iwth chat

--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Agent } from '../agents.service';
 
 @Component({
-  selector: 'app-chat-window',
-  standalone: true,
-  imports: [],
-  templateUrl: './chat-window.component.html',
-  styleUrl: './chat-window.component.scss'
+    selector: 'app-chat-window',
+    standalone: true,
+    imports: [],
+    templateUrl: './chat-window.component.html',
+    styleUrl: './chat-window.component.scss',
 })
 export class ChatWindowComponent {
-
+    @Input() agent?: Agent;
 }
