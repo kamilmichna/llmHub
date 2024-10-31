5b5aa7ebb25e32af8d7dc90214d415507a665f57
Kamil Michna <kamil.michna28@gmail.com>
2024-10-31 01:16:10 +0100

    modal for adding tickets

--- frontend/src/app/components/keys-view/keys-view.component.ts
+++ frontend/src/app/components/keys-view/keys-view.component.ts
@@ -18,17 +18,48 @@ export class KeysViewComponent {
         throw new Error('Method not implemented.');
     }
 
-    async openProviderModal() {
+    async addNewKey() {
         await Swal.fire({
             background: '#2a323c',
             color: '#a6adbb',
             html: `
-                <input id="api-key" class="swal2-input">
+                <div class='flex flex-col create-api-key'>
+                    <label for="name">Name:</label>
+                    <input id="name" class="swal2-input" required>
+                    <br><br>
+                    <label for="provider">Provider:</label>
+                    <select id="provider" class="swal2-input" required>
+                        <!-- assuming you have a list of providers, you can populate this select list dynamically -->
+                        <option value="">Select provider</option>
+                        <!-- add more options here -->
+                    </select>
+                    <br><br>
+                    <label for="api-key">API Key:</label>
+                    <input id="api-key" class="swal2-input" required> 
+                </div>
             `,
-            title: 'Delete Agent',
-            text: 'Are you sure that you want to delete this agent?',
-            icon: 'error',
-            confirmButtonText: 'Confirm',
+            title: 'Add New Key',
+            text: 'Enter new key details:',
+            confirmButtonText: 'Add',
+            preConfirm: () => {
+                const [name, apiKey] = [
+                    (
+                        document.querySelector(
+                            '.create-api-key #name'
+                        ) as HTMLInputElement
+                    ).value,
+                    (
+                        document.querySelector(
+                            '.create-api-key #api-key'
+                        ) as HTMLInputElement
+                    ).value,
+                ];
+
+                if (!apiKey?.length || !name?.length)
+                    Swal.showValidationMessage(
+                        '<i class="fa fa-info-circle"></i>All Fields are required!!!'
+                    );
+            },
         });
     }
 }
